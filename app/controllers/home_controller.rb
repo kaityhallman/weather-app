0d579abaf3948723dd836971e1354fd4a622587d
kaityhallman <kaitlinhallman@gmail.com>
2016-02-15 18:03:15 -0500

    added more in for response variable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,6 +20,14 @@ class HomeController < ApplicationController
     @feels_like = response["current_observation"]["feelslike_f"]
     else
         response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/NC/Asheville.json")
+        @location = response["location"]["city"]
+    @temp_f = response["current_observation"]["temp_f"]
+    @temp_c = response["current_observation"]["temp_c"]
+    @weather_icon = response["current_observation"]["icon_url"]
+    @weather_words = response["current_observation"]["weather"]
+    @forecast_link = response["current_observation"]["forecast_url"]
+    @feels_like = response["current_observation"]["feelslike_f"]
+
     end
        
 
