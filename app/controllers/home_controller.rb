52e0d68570a4ea6fa81b3ee4f4fbdd7a843a1db7
kaityhallman <kaitlinhallman@gmail.com>
2016-02-15 20:13:05 -0500

    sqlite3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,6 +11,11 @@ class HomeController < ApplicationController
     
     if params[:city] != nil
         response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json") 
+    else
+        response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/NC/Asheville.json")
+
+    end
+    
     @location = response["location"]["city"]
     @temp_f = response["current_observation"]["temp_f"]
     @temp_c = response["current_observation"]["temp_c"]
@@ -18,32 +23,20 @@ class HomeController < ApplicationController
     @weather_words = response["current_observation"]["weather"]
     @forecast_link = response["current_observation"]["forecast_url"]
     @feels_like = response["current_observation"]["feelslike_f"]
-    else
-        response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/NC/Asheville.json")
-        @location = response["location"]["city"]
-    @temp_f = response["current_observation"]["temp_f"]
-    @temp_c = response["current_observation"]["temp_c"]
-    @weather_icon = response["current_observation"]["icon_url"]
-    @weather_words = response["current_observation"]["weather"]
-    @forecast_link = response["current_observation"]["forecast_url"]
-    @feels_like = response["current_observation"]["feelslike_f"]
 
-    end
-       
-
-if @weather_words == "Overcast" || @weather_words == "Cloudy"
-    @url = "https://images.unsplash.com/photo-1445264618000-f1e069c5920f?crop=entropy&dpr=2&fit=crop&fm=jpg&h=775&ixjsv=2.0.0&ixlib=rb-0.3.5&q=50&w=1250"
-elsif @weather_words == "Clear" || @weather_words == "Sunny" || @weather_words == "Mostly Sunny"
-    @url = "https://images.unsplash.com/photo-1419133203517-f3b3ed0ba2bb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=23401e5526fc89877400795aef7e773e"
-elsif @weather_words == "Rain" || @weather_words == "Thunderstorms"
-    @url = "https://images.unsplash.com/photo-1428592953211-077101b2021b?crop=entropy&dpr=2&fit=crop&fm=jpg&h=775&ixjsv=2.0.0&ixlib=rb-0.3.5&q=50&w=1250"
-elsif @weather_words == "Snow" || @weather_words == "Sleet" || @weather_words == "Flurries"
-    @url = "https://images.unsplash.com/photo-1446463969211-28bf6e20b315?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=80a71632b3e71ffec32eb9195e0403cc"
-elsif @weather_words == "Fog" 
-    @url = "https://images.unsplash.com/photo-1446491429004-809880bd132e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=d8d94d86d4e498c13806418ba040969c"
-else 
-    @url = "https://images.unsplash.com/photo-1421081177127-339f586c9b49?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=f2d20cd9d3c70e91c0a2e2d35671a2f4"
-end
+      if @weather_words == "Overcast" || @weather_words == "Cloudy"
+          @url = "https://images.unsplash.com/photo-1445264618000-f1e069c5920f?crop=entropy&dpr=2&fit=crop&fm=jpg&h=775&ixjsv=2.0.0&ixlib=rb-0.3.5&q=50&w=1250"
+      elsif @weather_words == "Clear" || @weather_words == "Sunny" || @weather_words == "Mostly Sunny"
+          @url = "https://images.unsplash.com/photo-1419133203517-f3b3ed0ba2bb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=23401e5526fc89877400795aef7e773e"
+      elsif @weather_words == "Rain" || @weather_words == "Thunderstorms"
+          @url = "https://images.unsplash.com/photo-1428592953211-077101b2021b?crop=entropy&dpr=2&fit=crop&fm=jpg&h=775&ixjsv=2.0.0&ixlib=rb-0.3.5&q=50&w=1250"
+      elsif @weather_words == "Snow" || @weather_words == "Sleet" || @weather_words == "Flurries"
+          @url = "https://images.unsplash.com/photo-1446463969211-28bf6e20b315?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=80a71632b3e71ffec32eb9195e0403cc"
+      elsif @weather_words == "Fog" 
+          @url = "https://images.unsplash.com/photo-1446491429004-809880bd132e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=d8d94d86d4e498c13806418ba040969c"
+      else 
+          @url = "https://images.unsplash.com/photo-1421081177127-339f586c9b49?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=f2d20cd9d3c70e91c0a2e2d35671a2f4"
+      end
   end
 
   def test
